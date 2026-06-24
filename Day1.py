e710e2a0c0b91032b5f3c2aa20e4ed428082b86b
kkavipriyan1307-bit <kkavipriyan1307@gmail.com>
2026-06-24 18:53:46 +0530

    Add input checks and pattern printing in Day1.py
    Implement basic input checks for numbers, characters, and leap year determination. Add a simple pyramid pattern printing.

---
+++ Day1.py
@@ -0,0 +1,44 @@
+Day 1
+"""
+a=int(input())
+b=int(input())
+c=int(input())
+if(a==b and a>c):
+    print(" a and b is greater")
+elif(a==c and a>b):
+    print(" a and c is greater")
+elif(b==c and b>a):
+    print(" b and c is greater")
+elif(a>b and a>c):
+    print(" a is greater")
+elif(b>a and b>c):
+    print(b is greater)
+elif(a==b and b==c and a==c):
+    print("a,b,c is grester")
+else:
+    print(c is greater)
+"""
+"""
+a=input()
+if(ord(a)>=65 and ord(a)<=90):
+    print("uppercase")
+elif(ord(a)>=97 and ord(a)<=122):
+    print("Lowercase")
+elif(ord(a)>=48 and ord(a)<=57):
+    print("digit")
+else:
+    print("special character")
+"""
+"""
+a=int(input())
+if(a%4==0):
+    print(a, "is leap year")
+else:
+    print(a ,"is not leap year")
+"""
+"""
+n=3
+for i in range(1,4):
+    print("*" *i)
+"""
+
